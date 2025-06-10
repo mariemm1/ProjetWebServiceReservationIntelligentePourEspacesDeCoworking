6a17c7d4e056199eb4403cf82e49e481154458e7
nadagharbiiiiii <gharbinada60@gmail.com>
2025-06-10 13:39:59 +0200

    add JwtService,DTO,AuthGraphSLResolver and AuthService

---
+++ src/main/java/org/example/projetwebservice/DTO/AuthResponse.java
@@ -0,0 +1,14 @@
+package org.example.projetwebservice.DTO;
+
+public class AuthResponse {
+        private String token;
+
+        public AuthResponse(String token) {
+            this.token = token;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+    }
---
+++ src/main/java/org/example/projetwebservice/Services/AuthService.java
@@ -0,0 +1,59 @@
+package org.example.projetwebservice.Services;
+import org.example.projetwebservice.Config.JwtService;
+import org.example.projetwebservice.DTO.AuthRequest;
+import org.example.projetwebservice.DTO.AuthResponse;
+import org.example.projetwebservice.DTO.RegisterRequest;
+
+import org.example.projetwebservice.Model.Enum.Role;
+import org.example.projetwebservice.Model.User;
+import org.example.projetwebservice.Repository.UserRepository;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthService(UserRepository userRepository, JwtService jwtService,
+                       PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email déjà utilisé.");
+        }
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.USER); // automatiquement CLIENT
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user);
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse authenticate(AuthRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Utilisateur introuvable"));
+
+        String token = jwtService.generateToken(user);
+        return new AuthResponse(token);
+    }
+}
